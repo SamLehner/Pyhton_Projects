88832a0f7f009ff5644e6d56483e88b046506dcd
SamLehner <samblehner8@gmail.com>
2021-03-07 17:43:31 -0700

    Updated Django University Code
    Updated Code

--- DjangoUniversity/Classes/classApp/models.py
+++ DjangoUniversity/Classes/classApp/models.py
@@ -13,13 +13,8 @@ class DjangoClasses(models.Model):
     title = models.CharField(max_length=60, default="", blank=True, null=False)
     course_number = models.IntegerField(max_length=6, default=0, blank=True, null=False)
     instructor_name = models.CharField(max_length=60, default="", blank=True)
-    duration = models.FloatField(max_length=6, default=01.00, blank=True)
+    duration = models.FloatField(default=01.00, blank=True)
 
-    objects = ClassManager()
-
-
-class1 = DjangoClasses.objects.create_class("Coding 101", 1, "Jessica Smith", 01.30)
-class2 = DjangoClasses.objects.create_class("Django Fundamentals", 2, "Rob Davos", 02.00)
-class3 = DjangoClasses.objects.create_class("Python Basics", 3, "Billy Jean", 00.45)
+    objects = models.Manager()
 
 
